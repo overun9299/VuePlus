9ceddb8c437a205010448dcdf5ff18fffefe2a08
ZhangPY <18502973371@163.com>
2020-06-15 19:29:17 +0800

    首页布局

--- src/main.js
+++ src/main.js
@@ -10,9 +10,16 @@ import './assets/fonts/iconfont.css'
 // 引入全局axios
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+
 // 配置请求的跟路径
 axios.defaults.baseURL = 'http://127.0.0.1:9100/ms/user/'
 Vue.prototype.$http = axios
+
+// 设置axios全局拦截器
+axios.interceptors.request.use(config =>{
+  config.headers.token = window.sessionStorage.getItem("token")
+  return config
+})
 
 Vue.config.productionTip = false
 Vue.use(VueAxios,axios);
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,11 +1,51 @@
+// 按需导入element-ui组建
 import Vue from 'vue'
-// 按需导入组件
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import {
+    Button,
+    Form,
+    FormItem,
+    Input,
+    Message,
+    Container,
+    Header,
+    Aside,
+    Main,
+    Menu,
+    Submenu,
+    MenuItem,
+    Breadcrumb,
+    BreadcrumbItem,
+    Card,
+    Row,
+    Col,
+    Table,
+    TableColumn,
+    Switch,
+    Tooltip,
+    Pagination,
+    Dialog
+} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
-
-// 挂载element的消息提醒
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
 Vue.prototype.$message = Message
